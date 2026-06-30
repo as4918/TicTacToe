a9be96417ab6e055db08a94b5df68513bcb6a7de
as4918 <as4918@srmist.edu.in>
2026-06-30 21:30:14 +0530

    Implemented UC1: Display empty Tic-Tac-Toe board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,52 @@
+/**
+ * TicTacToe
+ * UC1 initializes and displays an empty Tic-Tac-Toe board.
+ */
+
 public class TicTacToe {
 
+    static char[][] board = new char[3][3];
+
     public static void main(String[] args) {
-        System.out.println("Project Created Successfully!");
+
+        initializeBoard();
+        printBoard();
+
+    }
+
+    static void initializeBoard() {
+
+        for (int row = 0; row < 3; row++) {
+
+            for (int col = 0; col < 3; col++) {
+
+                board[row][col] = '-';
+
+            }
+
+        }
+
+    }
+
+    static void printBoard() {
+
+        System.out.println("-------------");
+
+        for (int row = 0; row < 3; row++) {
+
+            System.out.print("| ");
+
+            for (int col = 0; col < 3; col++) {
+
+                System.out.print(board[row][col] + " | ");
+
+            }
+
+            System.out.println();
+            System.out.println("-------------");
+
+        }
+
     }
 
 }
