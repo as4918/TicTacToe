2585b18114b2d574a98beb099a23f3acb26f3285
as4918 <as4918@srmist.edu.in>
2026-07-01 08:51:19 +0530

    Implemented UC8: Continuous turn-based game loop

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,71 +1,34 @@
-import java.util.Random;
-
 /**
  * TicTacToe
- * UC7 allows the computer to make a random valid move.
+ * UC8 demonstrates a continuous turn-based game loop.
  */
 
 public class TicTacToe {
 
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
-
-    static char computerSymbol = 'O';
-
-    static Random random = new Random();
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
 
-        computerMove();
+        while (!gameOver) {
 
-        printBoard();
+            if (isHumanTurn) {
 
-    }
+                System.out.println("Human Turn");
 
-    static void computerMove() {
+            } else {
 
-        while (true) {
-
-            int slot = random.nextInt(9) + 1;
-
-            int row = (slot - 1) / 3;
-            int col = (slot - 1) % 3;
-
-            if (board[row][col] == '-') {
-
-                board[row][col] = computerSymbol;
-
-                System.out.println("Computer selected slot: " + slot);
-
-                break;
+                System.out.println("Computer Turn");
 
             }
 
+            isHumanTurn = !isHumanTurn;
+
+            // Temporary stop for UC8 demonstration
+            gameOver = true;
         }
 
-    }
-
-    static void printBoard() {
-
-        System.out.println("-------------");
-
-        for (int row = 0; row < 3; row++) {
-
-            System.out.print("| ");
-
-            for (int col = 0; col < 3; col++) {
-
-                System.out.print(board[row][col] + " | ");
-
-            }
-
-            System.out.println();
-            System.out.println("-------------");
-
-        }
+        System.out.println("Game Over");
 
     }
 
